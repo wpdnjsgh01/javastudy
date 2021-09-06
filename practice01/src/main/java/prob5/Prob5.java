fffeb4783d41912154441ad1d067283fa57ea7af
wpdnjsgh01 <wpdnjsgh02@naver.com>
2021-09-06 20:29:56 +0900

    reaplace2

--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -1,7 +1,17 @@
 package prob5;
-
+import java.util.*;
 public class Prob5 {
 
 	public static void main(String[] args) {
+		
+		for(int i = 0; i < 100; i++) {
+			if(i / 10 == 3 || i / 10 == 6 || i / 10 == 9) {
+				System.out.println(i + " 짝");
+			}
+			if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+				System.out.println(i + " 짝");
+			}
+		}
+		
 	}
 }
