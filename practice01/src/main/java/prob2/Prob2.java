61bd715f05033c5384250466ecf304eb0efa50a4
douzone-bipa <douzone.bipa@gmail.com>
2021-09-06 19:16:06 +0900

    1

---
+++ practice01/src/main/java/prob2/Prob2.java
@@ -0,0 +1,16 @@
+package prob2;
+import java.util.Arrays;
+
+public class Prob2 {
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 10; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print(j);
+			}
+		}
+		
+	
+		
+	}
+}
